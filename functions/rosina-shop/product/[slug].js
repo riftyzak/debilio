7ddaf8dce68f3579707857a4fff72dddd37ebe7c
unknown <krystof29altmann@gmail.com>
2026-01-20 21:50:18 +0100

    debilio shop update v3

--- functions/rosina-shop/product/[slug].js
+++ functions/rosina-shop/product/[slug].js
@@ -1,12 +1,16 @@
 export async function onRequestGet(context) {
-  const slug = context.params.slug; // <-- gets the [slug] from the URL
+  const slug = context.params.slug; // <-- from /product/<slug>
   const url = new URL(context.request.url);
 
-  // Rewrite to the static product page and pass slug as query param
+  // Serve the static product page but with ?slug=<slug>
   url.pathname = "/rosina-shop/product.html";
   url.search = "";
   url.searchParams.set("slug", slug);
 
-  // Fetch the static asset through Pages
-  return fetch(url.toString(), context.request);
+  // Use Pages asset fetch so it serves the built/static file
+  const res = await context.env.ASSETS.fetch(url.toString(), context.request);
+
+  const headers = new Headers(res.headers);
+  headers.set("content-type", "text/html; charset=utf-8");
+  return new Response(res.body, { status: res.status, headers });
 }
