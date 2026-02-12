ac01e046d38f86a4b80ca5d7eb2cf32c32b1b51d
riftyzak <krystof29altmann@gmail.com>
2026-02-12 19:11:00 +0100

    editor vim

--- functions/rosina-shop/success.html.js
+++ functions/rosina-shop/success.html.js
@@ -1,6 +1,6 @@
 import { serveAssetWithHeaders } from "../_lib/html-security.js";
 
-const SUCCESS_CSP = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; form-action 'self'";
+const SUCCESS_CSP = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data:; connect-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; form-action 'self'";
 
 export async function onRequestGet(context) {
   return serveAssetWithHeaders(context, "/rosina-shop/success.html", {
